8002c9129d5686bfce7c3c605973c333f6ccddfd
Hinterlock <102774385+Hinterlock@users.noreply.github.com>
2022-05-15 22:52:08 -0700

    added dialogue

--- src/scenes/Alleyway.js
+++ src/scenes/Alleyway.js
@@ -17,13 +17,45 @@ class Alleyway extends Phaser.Scene {
         this.cards.scale = .5;
 
         this.cursor = this.add.sprite(-100, -100, 'cursor');
+
+        //bg fade in dialogue
         this.fade = this.add.image(game.config.width/2, game.config.height/2, 'fade');
         this.fade.scale = 1.5;
 
+        //detective talksprite
         this.det = this.add.sprite(game.config.width/6, game.config.height*1/2, 'det_neut');
         this.det.scale = .2;
         this.det.y = game.config.height - this.det.height*this.det.scale/2;
         this.det.x = -this.det.width*this.det.scale;
+
+        //text formatting (please change)
+        this.textConfig = {
+            fontFamily: 'Courier',
+            fontSize: '24px'
+            //color: ''
+        };
+        this.text = this.add.text(game.config.width*3/8, game.config.height/8, '', this.textFormat);
+
+        //json object containing dialogue: in future will move to separate file thats loaded in
+        this.dialogue1 = [
+            {
+                'text': ["A B C",
+                        "1 2 3 4 5"],
+                'speaker': 'det'
+            },
+            {
+                'text': ["yay",
+                        "a a b",
+                        ""],
+                'speaker': 'someone else lol'
+            }
+        ];
+        this.dialogue = this.dialogue1;
+        this.textBox = 0;
+        this.wordDelay = 100;
+        this.timer;
+
+        //console.log(this.dialogue[this.lineIndex].text);
 
         this.tweens.add({
             targets: this.fade,
@@ -35,7 +67,9 @@ class Alleyway extends Phaser.Scene {
             targets: this.det,
             x: { from: this.det.x, to: game.config.width/6},
             ease: 'Sine.easeOut',
-            duration: 1000
+            duration: 1000,
+            onComplete: this.nextBox(),
+            onCompleteScope: this
         });
 
         this.state = 0;
@@ -43,24 +77,66 @@ class Alleyway extends Phaser.Scene {
         keySPACE.on('down', this.space, this);
     }
 
-    space() {
-        if (this.state) {
-
+    nextBox() {
+        this.text.text = '';
+        if (this.dialogue[this.textBox].speaker == 'det') {
+            this.text.x = game.config.width*2/8;
         } else {
-            this.tweens.add({
-                targets: this.fade,
-                alpha: { from: 1, to: 0},
-                ease: 'Sine.easeIn',
-                duration: 1000
-            });
-            this.tweens.add({
-                targets: this.det,
-                x: { from: this.det.x, to: -this.det.width*this.det.scale},
-                ease: 'Sine.easeIn',
-                duration: 1000
-            });
+            this.text.x = game.config.width/2;
         }
-        this.state = 1;
+        this.nextLine(0);
+    }
+    nextLine(lineIndex) {
+        if (lineIndex == this.dialogue[this.textBox].text.length) {
+            this.textBox += 1;
+            return;
+        }
+        let wordIndex = 0;
+        let line = this.dialogue[this.textBox].text[lineIndex].split(' ');
+        this.timer = this.time.addEvent({
+            delay: this.wordDelay,
+            callback: function(){
+                this.nextWord(line, wordIndex, lineIndex);
+                wordIndex++;
+            },
+            callbackScope: this,
+            repeat: line.length
+        });
+    }
+    nextWord(line, wordIndex, lineIndex) {
+        if (wordIndex == line.length) {
+            this.text.text = this.text.text.concat("\n");
+            this.nextLine(lineIndex+1);
+        } else {
+            this.text.text = this.text.text.concat(line[wordIndex] + " ");
+        }
+    }
+
+    space() {
+        if (!this.state){
+            if (this.timer.getOverallRemaining() > 0) {
+                this.timer.remove(false);
+                this.textBox ++;
+            }
+            if (this.textBox < this.dialogue.length) {
+                this.nextBox();
+            } else {
+                this.text.text = '';
+                this.tweens.add({
+                    targets: this.fade,
+                    alpha: { from: 1, to: 0},
+                    ease: 'Sine.easeIn',
+                    duration: 1000
+                });
+                this.tweens.add({
+                    targets: this.det,
+                    x: { from: this.det.x, to: -this.det.width*this.det.scale},
+                    ease: 'Sine.easeIn',
+                    duration: 1000
+                });
+                this.state = 1;
+            }
+        }
     }
 
     update() {
