b894f7fffc8e5f60beed9eebd15de000fb484481
Lee Coronilia <shadowwolf246815@gmail.com>
2022-05-16 22:42:10 -0700

    new street bgs

--- src/scenes/Street.js
+++ src/scenes/Street.js
@@ -3,18 +3,25 @@ class Street extends Phaser.Scene {
         super("streetScene");
     }
     preload() {
-        this.load.image('road', './assets/road.png');
+        this.load.image('buildings', './assets/buildings.png');
+        this.load.image('alleyway', './assets/alleyway.png');
+        this.load.image('bar', './assets/bar.png');
+        this.load.image('bakery', './assets/bakery.png');
         this.load.image('street', './assets/street.png');
         this.load.image('detective', './assets/detective.png');
     }
     create() {
         this.cameras.main.setBackgroundColor('#42f557');
         
-        this.street = this.add.image(game.config.width + 100, game.config.height* 3/4 , 'street');
-        this.road = this.add.image(game.config.width + 90, game.config.height * 3/4, 'road');
+        this.buildings = this.add.image(game.config.width - 70, game.config.height * 1/2, 'buildings');
+        this.bar = this.add.image(game.config.width - 70, game.config.height * 1/2, 'bar');
+        this.alleyway = this.add.image(game.config.width - 70, game.config.height * 1/2, 'alleyway');
+        this.bakery = this.add.image(game.config.width - 70, game.config.height * 1/2, 'bakery');
+        this.street = this.add.image(game.config.width - 70, game.config.height * 1/2, 'street');
+        // this.street = this.add.image(game.config.width + 90, game.config.height * 1/2, 'street');
         this.detective = this.add.sprite(enter, game.config.height*9/16,'detective');
         if (enter != 50) {
-            this.detective.y = game.config.height*3/8;
+            this.detective.y = game.config.height*3/8; 
             this.detective.setScale(0.90);
         }
         
@@ -57,7 +64,7 @@ class Street extends Phaser.Scene {
             //this.move = true;
             this.moveTo(this.detective, this.input.activePointer.x, this.input.activePointer.y - this.detective.height*3/8);
         }
-        if (this.detective.x < this.road.width - cam.width/2 && this.detective.x > cam.width/2) {
+        if (this.detective.x < this.street.width - cam.width/2 && this.detective.x > cam.width/2) {
             cam.scrollX = this.detective.x - cam.width/2;
         }
     }
