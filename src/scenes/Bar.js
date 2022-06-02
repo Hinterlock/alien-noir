fdd660dfa369d576e12773588b0794d2a46c4420
Lee Coronilia <shadowwolf246815@gmail.com>
2022-06-02 14:48:24 -0700

    made bar and set correct positions

--- src/scenes/Bar.js
+++ src/scenes/Bar.js
@@ -3,7 +3,15 @@ class Bar extends baseScene {
         super("barScene");
     }
     preload() {
-        this.load.image('bar_interior', './assets/bar_interior.png');
+        //images
+        this.load.image('bar_bg', './assets/bar/bar_bg.png');
+        this.load.image('aliens', './assets/bar/aliens.png');
+        this.load.image('stage', './assets/bar/stage.png');
+        this.load.image('bartender', './assets/bar/bartender.png');
+        this.load.image('bartender_outlined', './assets/bar/bartender_outlined.png');
+        this.load.image('stage_outlined', './assets/bar/stage_outlined.png');
+        this.load.image('aliens_outlined', './assets/bar/aliens_outlined.png');
+        //text
         this.load.json('bar1_intro', './assets/text/bar1.json');
         this.load.json('bar2', './assets/text/bar2.json');
         this.load.json('bar3', './assets/text/bar3.json');
@@ -11,7 +19,10 @@ class Bar extends baseScene {
         this.load.json('bar5', './assets/text/bar5.json');
     }
     create() {
-        this.bg = this.add.image(game.config.width/2, game.config.height/2, 'bar_interior');
+        this.barbg = this.add.image(game.config.width/2, game.config.height/2, 'bar_bg');
+        this.stage = this.add.image(game.config.width/3.7, game.config.height*1/3, 'stage');
+        this.aliens = this.add.image(game.config.width/2, game.config.height*.56, 'aliens');
+        this.bartender = this.add.image(game.config.width*6/8.3, game.config.height*1/2.6, 'bartender');
         
         this.setup();
         this.input.on('pointerdown', function() {
