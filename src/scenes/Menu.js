a6ce1f1eb9540fcb2f6055f4dfb200c5935e2945
Hinterlock <102774385+Hinterlock@users.noreply.github.com>
2022-06-03 15:46:50 -0700

    added menu images

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -21,13 +21,26 @@ class Menu extends Phaser.Scene {
         this.load.image('borders', './assets/borders.png');
         this.load.image('box', './assets/textbox.png');
 
+        // load menu images
+        this.load.image('menuBg', './assets/menu/title_screen.png');
+        this.load.image('credits_button', './assets/menu/credits_button.png');
+        this.load.image('start_button', './assets/menu/start_button.png');
+
         // load font
         this.load.bitmapFont('gem_font', './assets/font/gem.png', './assets/font/gem.xml');
     }
 
     create() {
+        this.add.image(game.config.width/2, game.config.height/2, 'menuBg');
         this.add.text(20, 20, 'Click to start, use the mouse to investigate and move around the neighborhood');
+        
+        this.menuBttn = this.add.image(game.config.width/2, game.config.height*2/3, 'start_button');
+        this.creditsBttn = this.add.image(game.config.width/2, game.config.height*5/6, 'credits_button');
         //this.title = this.add.image(game.config.width/2, game.config.height/2, 'title');
+
+        this.wipe = this.add.rectangle(game.config.width/2, game.config.height/2, game.config.width, game.config.height, game.config.backgroundColor._color);
+        this.wipe.alpha = 0;
+
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keySPACE.on('down', function() {
             for (let n in gameProgress['streetScene']) {
@@ -38,10 +51,34 @@ class Menu extends Phaser.Scene {
             this.sound.play('click');
         }, this);
         this.input.on('pointerdown', function() {
-            this.scene.start("alleyWayScene");
-            this.sound.play('startup');
+            if (this.checkMouseOver(this.input.activePointer, this.menuBttn)) {
+                this.wipe.alpha = 1;
+                this.tweens.add({
+                    targets: this.wipe,
+                    x: { from: this.cameras.main.scrollX - game.config.width/2, to: this.cameras.main.scrollX + game.config.width/2},
+                    ease: 'Sine.easeIn',
+                    duration: 500,
+                    onComplete: function() {
+                        this.scene.start("alleyWayScene");
+                        this.sound.play('startup');
+                    },
+                    onCompleteScope: this
+                });
+            }
         }, this);
     }
     
     update(){}
+    
+    checkMouseOver(mouse, sprite) {
+        // simple AABB checking
+        if (mouse.x + this.cameras.main.scrollX < sprite.x + sprite.width/2 && 
+            mouse.x + this.cameras.main.scrollX > sprite.x - sprite.width/2 &&
+            mouse.y < sprite.y + sprite.height/2 &&
+            mouse.y > sprite.y - sprite.height/2) {
+                return true;
+        } else {
+            return false;
+        }
+    }
 }
